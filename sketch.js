62b486e2c49e925a9ea66e0e5cc9c6b9601d0547
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-03-08 12:18:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+ const Engine = Matter.Engine;
+ const World = Matter.World;
+ const Bodies = Matter.Bodies;
+ const Constraint = Matter.Constraint;  
+ var engine,world;
+ var man_img,thunder1,thunder2,thunder3,thunder4;
+ var drop,maxDrops,drops = [];
+
+function preload(){
+    man_img = loadImage("walking_1.png","walking_2.png","walking_3.png","walking_4.png","walking_5.png","walking_6.png","walking_7.png","walking_8.png");
+    thunder1 = loadImage("1.png");
+    thunder2 = loadImage("2.png");
+    thunder3 = loadImage("3.png");
+    thunder4 = loadImage("4.png");
+}
+
+function setup(){
+   createCanvas(400,600);
+
+   engine = Engine.create();
+   world = engine.world;
+
+   Engine.run(engine);
+}
+
+function draw(){
+    background(0);
+
+    Engine.update(engine);
+
+    maxDrops = 100;
+
+    for(var i=0;i<maxDrops;i++) {
+       drops.push(new Drop(random(0,400),random(0,800)));
+       drops[i].display();
+    }
+
+    image(man_img,10,250,370,370);
+
+    if(frameCount%40===0) {
+       rand = Math.round(random(1,4));
+       thunder = createSprite(random(10,370),random(10,30,10,10));
+       thunder.lifetime = 2;
+       switch(rand) {
+           case 1: thunder.addImage(thunder1);
+           break;
+
+           case 2: thunder.addImage(thunder2);
+           break;
+
+           case 3: thunder.addImage(thunder3);
+           break;
+           
+           case 4: thunder.addImage(thunder4);
+           break;
+
+           default: break;
+       }
+       thunder.scale = random(0.3,0.6);
+    } 
+
+    console.log(frameCount);
+
+       drawSprites();
+}
